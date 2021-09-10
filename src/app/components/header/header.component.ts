a926d0dd9313cb05ab6e0a288fd7b08a4b7c7e1e
Zydrunas <zbutrimas@gmail.com>
2021-09-10 10:10:34 +0300

    nav-item change color

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,7 +4,7 @@ import {Component, OnInit} from '@angular/core';
   selector: 'app-header',
   template: `
     <header>
-      <nav class="navbar navbar-expand-xl navbar-dark static-top bg-dark">
+      <nav class="navbar navbar-expand-xl navbar-dark static-top bg-dark ">
         <div class="container-fluid">
         <a class="navbar-brand mx-3">
           <img src="https://icon-library.com/images/to-do-list-icon/to-do-list-icon-10.jpg"
@@ -21,11 +21,11 @@ import {Component, OnInit} from '@angular/core';
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
           <ul class="navbar-nav mr-auto">
-            <li [routerLinkActive]="['active']" class="nav-item">
-              <a class="nav-link" [routerLink]="['/home']">Home</a>
+            <li [routerLinkActive]="['active']" class="nav-item fs-5">
+              <a class="nav-link " [routerLink]="['/home']" >Home</a>
             </li>
-            <li [routerLinkActive]="['active']" class="nav-item">
-              <a class="nav-link" [routerLink]="['/todoapps']">Todo apps</a>
+            <li [routerLinkActive]="['active']" class="nav-item fs-5" >
+              <a class="nav-link" [routerLink]="['/todoapps']" >Todo apps</a>
             </li>
           </ul>
         </div>
